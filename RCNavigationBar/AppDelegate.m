08f1ce6eb406db0ab7284f15cc6e9721123bbb89
Ren Chao <>
2020-02-23 16:51:42 +0800

    navigationBar 常用设置属性

--- RCNavigationBar/AppDelegate.m
+++ RCNavigationBar/AppDelegate.m
@@ -18,7 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    self.window.rootViewController = [[UIViewController alloc]init];
+    ViewController *VC = [[ViewController alloc]init];
+    UINavigationController *mainNaviController = [[UINavigationController alloc]initWithRootViewController:VC];
+    self.window.rootViewController = mainNaviController;
     [self.window makeKeyWindow];
     return YES;
 }
@@ -27,18 +29,18 @@
 #pragma mark - UISceneSession lifecycle
 
 
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
-}
-
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
+//- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+//    // Called when a new scene session is being created.
+//    // Use this method to select a configuration to create the new scene with.
+//    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+//}
+//
+//
+//- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+//    // Called when the user discards a scene session.
+//    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+//    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//}
 
 
 @end
--- RCNavigationBar/ViewController.m
+++ RCNavigationBar/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "NavigationViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UIButton *naviButton;
 
 @end
 
@@ -17,7 +20,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor purpleColor];
+    self.naviButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.naviButton.frame = CGRectMake(100, 100, 100, 100);
+    self.naviButton.backgroundColor = [UIColor redColor];
+    [self.naviButton addTarget:self action:@selector(jumpToNaiviVC) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.naviButton];
+    
+    //右侧item
+    UIButton *leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    leftButton.frame = CGRectMake(0, 100, 100, 100);
+    [leftButton setTitle:@"right" forState:UIControlStateNormal];
+    [leftButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    
+    UIBarButtonItem *leftItem = [[UIBarButtonItem alloc]initWithCustomView:leftButton];
+    self.navigationItem.rightBarButtonItem = leftItem;
+    
+    //Title
+    self.navigationItem.title = @"title";
+//    self.navigationItem.prompt = @"prompt";
+    
 }
 
-
+- (void)jumpToNaiviVC {
+    NavigationViewController *naviVC = [[NavigationViewController alloc]init];
+//    UIBarButtonItem *backBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"" style:UIBarButtonItemStyleDone target:nil action:nil];
+//    self.navigationItem.backBarButtonItem = backBarButtonItem;
+    [self.navigationController pushViewController:naviVC animated:nil];
+    
+}
 @end
